029ece495de7793a82c4b6d76288c9322165a91a
Keagan <esorts.k990@gmail.com>
2020-11-27 00:22:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+ //route handler
+app.get('/',(req, res) => {
+res.send('Hello World!!!!!')
+});
+
+app.get('/api/courses', (req, res) => {
+res.send([1,2,3])
+});
+
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
